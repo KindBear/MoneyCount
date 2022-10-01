1be7ccaa9ec1e9d417377fecf7f2fd30fb0868c3
Andrii Honchar <honchar.andrii@gmail.com>
2022-10-01 13:52:10 +0300

    Add Accunts Page

--- src/renderer/navRoutes.tsx
+++ src/renderer/navRoutes.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ViewListSharpIcon from "@mui/icons-material/ViewListSharp";
 import HomeIcon from "@mui/icons-material/Home";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import MainPage from "./pages/MainPage";
 import TransactionsPage from "./pages/TransactionsPage";
 import SettingsPage from "./pages/SettingsPage";
+import AccountsPage from "./pages/AccountsPage";
 
 export const NAV_ROUTES = [
     {
@@ -20,6 +22,13 @@ export const NAV_ROUTES = [
     to: "/transactions",
     component: <TransactionsPage/>,
     match: ["/transactions"],
+  },
+  {
+    title: "Accounts",
+    icon: <AccountBalanceWalletIcon />,
+    to: "/accounts",
+    component: <AccountsPage/>,
+    match: ["/accounts"],
   },
   {
     title: "Settings",
