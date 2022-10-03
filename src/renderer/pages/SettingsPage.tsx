099411613d88b8303891dcc240eaf9a2f3f1b26a
vadosxist@gmail.com <vadosxist@gmail.com>
2022-10-03 21:23:10 +0300

    Add_filters

--- src/renderer/pages/SettingsPage.tsx
+++ src/renderer/pages/SettingsPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Tabs  from "@mui/material/Tabs";
+import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -16,11 +16,23 @@ const SettingsPage = () => {
         sx={{
           height: "48px",
           paddingLeft: "8px",
+          position: "relative",
+          top: "-16px",
         }}
       >
         <Tabs value={currentTab}>
-          <Tab label="General" value="/settings" to="/settings" component={Link} />
-          <Tab label="Categories" value="/settings/categories" to="/settings/categories" component={Link} />
+          <Tab
+            label="General"
+            value="/settings"
+            to="/settings"
+            component={Link}
+          />
+          <Tab
+            label="Categories"
+            value="/settings/categories"
+            to="/settings/categories"
+            component={Link}
+          />
         </Tabs>
       </Box>
       <Box
